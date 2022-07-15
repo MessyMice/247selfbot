f8cd1d71437dc93d04b528318678a1ee209f433e
MessDaMice <88423770+MessyMice@users.noreply.github.com>
2022-07-15 19:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.login(TOKEN)
 //express
 const express = require("express")
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get(`/`, (req, res) => res.send(`Yo!`))
 
